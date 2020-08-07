1c61872c78b6ca6440e32070cfb26b27fb1c4fc9
fmount <fmount@inventati.org>
2020-08-07 16:33:45 +0200

    Update dotfiles with latest changes
    Signed-off-by: fmount <fmount@inventati.org>

--- scripts/i3scripts/dock.sh
+++ scripts/i3scripts/dock.sh
@@ -4,9 +4,13 @@ MODE=("auto" "off")
 CMD="$(command -v xrandr)"
 
 PRIMARY="eDP1"
-TARGET="DP2"
+TARGET="HDMI2"
 
+filter(){
+    TARGET=$("$CMD" | awk '/ connected/ {print $1}' | grep -v "$PRIMARY")
+}
 
+filter
 case "$1" in
     "dock")
         ${CMD} --output "${PRIMARY}" --"${MODE[1]}" --output "${TARGET}" --"${MODE[0]}" \
