3974a598cded428f8144bafb65000af7e89a18e5
fmount <fmount@inventati.org>
2020-09-17 11:53:15 +0200

    Update dotfiles with new experimental functions
    This change is supposed to bring:
    
    1. some cleanup on the coding style used in my dotfiles
       (which is sometimes insane)
    
    2. new experimental tmux functions to change the prefix
       when a ssh active session is detected
    
    Signed-off-by: fmount <fmount@inventati.org>

--- .zsh/arch-git-prompt.zsh
+++ .zsh/arch-git-prompt.zsh
@@ -46,9 +46,9 @@ send_tmux() {
 }
 
 _commit_count() {
-	count="$(command git rev-list --count HEAD 2>/dev/null)"
+    count="$(command git rev-list --count HEAD 2>/dev/null)"
     [ -z "$count" ] && count=0
-	echo "$count"
+    echo "$count"
 }
 
 is_ahead(){
@@ -56,64 +56,64 @@ is_ahead(){
         echo "%F{yellow}(INIT_REPO)%F{grey}"
         return
     fi
-	remote_branch="$(command git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)"
-	if [ -n "$remote_branch" ]; then
-		ah0="$(command git rev-list --count --left-right $remote_branch...$prompt_local_branch | awk '{print $1}')"
-		ah1="$(command git rev-list --count --left-right $remote_branch...$prompt_local_branch | awk '{print $2}')"
-		[ ${ah0} -ne 0 ] && echo "${orange}$GIT_PULL_ARROW${reset}${cyan}$ah0${reset}"
-		[ ${ah1} -ne 0 ] && echo "${orange}$GIT_PUSH_ARROW${reset}${cyan}$ah1${reset}"
-	else
-		echo "%F{yellow}$ERROR_FETCH_REMOTE_INFO%F{grey}"
-	fi
+    remote_branch="$(command git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)"
+    if [ -n "$remote_branch" ]; then
+        ah0="$(command git rev-list --count --left-right $remote_branch...$prompt_local_branch | awk '{print $1}')"
+        ah1="$(command git rev-list --count --left-right $remote_branch...$prompt_local_branch | awk '{print $2}')"
+        [ ${ah0} -ne 0 ] && echo "${orange}$GIT_PULL_ARROW${reset}${cyan}$ah0${reset}"
+        [ ${ah1} -ne 0 ] && echo "${orange}$GIT_PUSH_ARROW${reset}${cyan}$ah1${reset}"
+    else
+        echo "%F{yellow}$ERROR_FETCH_REMOTE_INFO%F{grey}"
+    fi
 }
 
 
 is_dirty(){
-	if [ -n "$(command git status --porcelain)" ]; then
-		echo "%F{grey}[%F{yellow}$GIT_BRANCH_CHANGED_SYMBOL%F{grey}$(is_ahead)|C:$(_commit_count)%{$reset_color%}]%F{grey}"
-	elif [ -z "$(is_ahead)" ]; then
-		echo "%F{grey}[%F{grey}$(_commit_count)%F{grey}%{$reset_color%}]%F{grey}"
-		#echo "  "
-	else
-		echo "%F{grey}[%F{grey}$(is_ahead)|C:$(_commit_count)%{$reset_color%}]%F{grey}"
-	fi
-	#remote_branch="$(command git rev-parse --abbrev-ref --symbolic-full-name @{u})"
-	#[ -n "$(command git log $remote_branch...HEAD)" ] && echo "%F{yellow}$GIT_BRANCH_CHANGED_SYMBOL$(is_ahead)%F{grey}"
+    if [ -n "$(command git status --porcelain)" ]; then
+        echo "%F{grey}[%F{yellow}$GIT_BRANCH_CHANGED_SYMBOL%F{grey}$(is_ahead)|C:$(_commit_count)%{$reset_color%}]%F{grey}"
+    elif [ -z "$(is_ahead)" ]; then
+        echo "%F{grey}[%F{grey}$(_commit_count)%F{grey}%{$reset_color%}]%F{grey}"
+        #echo "  "
+    else
+        echo "%F{grey}[%F{grey}$(is_ahead)|C:$(_commit_count)%{$reset_color%}]%F{grey}"
+    fi
+    #remote_branch="$(command git rev-parse --abbrev-ref --symbolic-full-name @{u})"
+    #[ -n "$(command git log $remote_branch...HEAD)" ] && echo "%F{yellow}$GIT_BRANCH_CHANGED_SYMBOL$(is_ahead)%F{grey}"
 }
 
 
 _settitle() {
-	printf "\033k$1\033\\"
+    printf "\033k$1\033\\"
 }
 
 
 check_git(){
-	belongs_to_repo=$(command git rev-parse --git-dir 2>/dev/null)
-	if [ -d "$belongs_to_repo"  ]; then
-		prompt_local_branch="$(command git symbolic-ref -q HEAD | sed -e 's|^refs/heads/||')"
+    belongs_to_repo=$(command git rev-parse --git-dir 2>/dev/null)
+    if [ -d "$belongs_to_repo"  ]; then
+        prompt_local_branch="$(command git symbolic-ref -q HEAD | sed -e 's|^refs/heads/||')"
         send_tmux $prompt_local_branch
-		#_settitle "$*"
-		echo "(${heavenly}$prompt_local_branch%{$reset_color%}% %F{grey})$(is_dirty)%F{grey}"
-	else
-		prompt_local_branch="$(command git rev-parse --git-dir > /dev/null 2>&1)"
+        #_settitle "$*"
+        echo "(${heavenly}$prompt_local_branch%{$reset_color%}% %F{grey})$(is_dirty)%F{grey}"
+    else
+        prompt_local_branch="$(command git rev-parse --git-dir > /dev/null 2>&1)"
         send_tmux
-		#_settitle "$*"
-	fi
+        #_settitle "$*"
+    fi
 }
 
 
 check_path() {
-	path=$(pwd)
-	IFS='/' read -A mypath <<< "$path"
-	if [ "${#mypath[@]}" -gt "$PLEN" ]; then
-		if [ "${mypath[2]}" != 'home' ]; then
-			printf "\[..] %s/%s" "$mypath[-2]" "$mypath[-1]"
-		else
-			printf "\%s %s/%s" "$ZSH_SYMBOL_HOME" "$mypath[-2]" "$mypath[-1]"
-		fi
-	else
-		printf "%s" "~"
-	fi
+    path=$(pwd)
+    IFS='/' read -A mypath <<< "$path"
+    if [ "${#mypath[@]}" -gt "$PLEN" ]; then
+        if [ "${mypath[2]}" != 'home' ]; then
+            printf "\[..] %s/%s" "$mypath[-2]" "$mypath[-1]"
+        else
+            printf "\%s %s/%s" "$ZSH_SYMBOL_HOME" "$mypath[-2]" "$mypath[-1]"
+        fi
+    else
+        printf "%s" "~"
+    fi
 }
 
 
