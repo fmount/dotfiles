cfa3ac22a74f32fd5412ff8c66c8145a3fff3ce1
fmount <fmount@inventati.org>
2020-09-17 11:53:15 +0200

    Update dotfiles with new experimental functions
    This change is supposed to bring:
    
    1. some cleanup on the coding style used in my dotfiles
       (which is sometimes insane)
    
    2. new experimental tmux functions to change the prefix
       when a ssh active session is detected
    
    Signed-off-by: fmount <fmount@inventati.org>

--- .zsh/function.zsh
+++ .zsh/function.zsh
@@ -4,8 +4,20 @@
 #
 #
 
+_set_tmux_prefix() {
+    if [[ -z $(ss | grep -i ssh) ]] then
+        # No SSH sessions should be active
+        tmux set-option -g prefix Escape
+    else
+        # SSH session is there, we want
+        # backtick as prefix to avoid
+        # overlapping w/ (N)VIM escape
+        tmux set-option -g prefix '`'
+    fi
+}
+
 _settitle() {
-	printf "\033k$1\033\\"
+    printf "\033k$1\033\\"
 }
 
 ssh() {
@@ -16,45 +28,34 @@ ssh() {
 
 # Old LaTeX Building
 texbuild(){
-        filename=$(basename "$1")
-        extension="${filename##*.}"
-        filename="${filename%.*}"
-        if [ -f "$1" ]; then
-                latex $filename
-                latex $filename
-                echo "Making dvi.."
-                dvips $filename.dvi #-o $filename.ps
-                echo "Making ps.."
-                ps2pdf $filename.ps
-                echo "Making pdf"
-                rm *.log *.ps *.dvi *.out *.aux
-                #evince $filename.pdf
-        fi
+    filename=$(basename "$1")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    if [ -f "$1" ]; then
+        latex $filename
+        latex $filename
+        echo "Making dvi.."
+        dvips $filename.dvi #-o $filename.ps
+        echo "Making ps.."
+        ps2pdf $filename.ps
+        echo "Making pdf"
+        rm *.log *.ps *.dvi *.out *.aux
+    fi
 }
 
-# PASS EXPORT THE CORRENT RING ##
-
+# pass function to select the specified ring
 swapring(){
 
-        if [[ -z "$1" ]]; then
-                echo "Usage: swapring /path/to/ring"
-        else
-                export PASSWORD_STORE_DIR=$1
-    export PASSWORD_STORE_GIT=$1
-        fi
+    if [[ -z "$1" ]]; then
+        echo "Usage: swapring /path/to/ring"
+    else
+        export PASSWORD_STORE_DIR=$1
+        export PASSWORD_STORE_GIT=$1
+    fi
 
 }
 
 function aping {
-	if ! ping -c1 -w 5 "$1" &>/dev/null; then echo "Host is down"; else echo "isalive"; fi
-}
-
-#task list and summary
-tl() {
-	if [[ -z "$1" ]]; then
-		task list && task summary
-	else
-		task list project:$1 && task summary project:$1
-	fi
+    if ! ping -c1 -w 5 "$1" &>/dev/null; then echo "Host is down"; else echo "isalive"; fi
 }
 
