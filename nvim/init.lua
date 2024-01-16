835ec98ba5422e2ba98afddd41f2603acf6ab353
fmount <francesco.pan001@gmail.com>
2024-01-16 10:00:30 +0100

    Add nsstat function and update nvim alias

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,8 @@
 
 -- source a vimscript file
 vim.cmd('source ~/.config/nvim/vimrc/init.vim')
+
+-- colours and fonts
+vim.opt.background = 'dark'
+vim.cmd('colorscheme jellybeans')
+vim.cmd('syntax enable')
