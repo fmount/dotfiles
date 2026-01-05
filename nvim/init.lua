4dc88b56d0252a356b3958b7e3941e04a657ef6a
fmount <francesco.pan001@gmail.com>
2026-01-05 14:18:48 +0100

    Cleanup nvim config
    - Remove unused and redundant code
    - Better organizing files
    - keep vimrc basic settings to make the config work on remote servers
    - Add keymaps.lua to extend neovim specific keymappings
    - experimental Claude code integration
    - Fixed LSP config and the related autocomplete framework
    
    Signed-off-by: fmount <francesco.pan001@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,9 +3,5 @@
 -- source a vimscript file
 vim.cmd('source ~/.config/nvim/vimrc/init.vim')
 
--- colours and fonts
-vim.opt.background = 'dark'
-vim.cmd('colorscheme jellybeans')
-vim.cmd('syntax enable')
-vim.o.colorcolumn = '80'
 vim.g.startify_custom_header = ''
+vim.keymap.set('n', '<leader>cc', '<cmd>ClaudeCode<CR>', { desc = 'Toggle Claude Code' })
